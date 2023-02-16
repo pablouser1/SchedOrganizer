2ce8ec7ddd7a7ec293eb307d406e30b9a77d6803
Pablo Ferreiro <pferreiromero@gmail.com>
2023-02-16 17:24:55 +0100

    Fixed new line

--- src/components/ExtraHandler.tsx
+++ src/components/ExtraHandler.tsx
@@ -4,12 +4,15 @@ import Extra from "../interfaces/Extra";
 const ExtraHandler: Component<{extra: Extra[]}> = props => {
   return (
     <For each={props.extra}>{item =>
-      item.type === "url" ? <a href={item.data} target="_blank">{item.title}</a> : (
-        <>
-          <strong>{item.title}</strong>
-          <p class="new-line">{item.data}</p>
-        </>
-      )
+      <p class="new-line">
+        {item.type === "url" ? <a href={item.data} target="_blank">{item.title}</a> : (
+          <>
+            <strong>{item.title}</strong>
+            <br />
+            {item.data}
+          </>
+        )}
+      </p>
     }</For>
   )
 }
