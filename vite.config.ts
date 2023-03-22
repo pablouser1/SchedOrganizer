4e1935c4b9849ab2021617fe1a42efa8b06cc99b
Pablo Ferreiro <pferreiromero@gmail.com>
2023-03-22 11:21:50 +0100

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      devOptions: {
+        enabled: true
+      }
+    })
+  ],
   server: {
     port: 3000,
   },
