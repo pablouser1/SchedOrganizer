60cbfb8133485e6f757eda28bea238ed48542250
Pablo Ferreiro <me@pabloferreiro.es>
2023-05-09 20:13:24 +0200

    better sw

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,34 @@ export default defineConfig({
   plugins: [
     solidPlugin(),
     VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      includeAssets: [
+        'favicon.ico',
+        'favicon-16x16.png',
+        'favicon-32x32.png',
+        'apple-touch-icon.png',
+        'android-chrome-192x192.png',
+        'android-chrome-512x512.png'
+      ],
+      manifest: {
+        name: 'Schedule Organizer',
+        short_name: 'SchedOrganizer',
+        description: 'Organize your schedules easily',
+        theme_color: '#ffffff',
+        orientation: "portrait",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ],
       },
+      registerType: 'autoUpdate',
       devOptions: {
         enabled: true
       }
