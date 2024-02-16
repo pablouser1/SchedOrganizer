2ef60f85fb184befff283e4bf4532b2bc5ec2381
Pablo Ferreiro <me@pabloferreiro.es>
2024-02-16 13:58:21 +0100

    highlight current weekday

--- src/components/Weekdays.tsx
+++ src/components/Weekdays.tsx
@@ -2,11 +2,13 @@ import { For } from "solid-js";
 import WEEKDAYS from "../constants/weekdays";
 
 export default function Weekdays() {
+  const now = new Date()
+
   return (
     <div class="grid two-cols">
       <For each={WEEKDAYS}>{(day, i) => {
         return (
-          <article>
+          <article classList={{focused: now.getDay() === i()}}>
             <a href={`/weekdays/${i()}`}>{day}</a>
           </article>
         )
