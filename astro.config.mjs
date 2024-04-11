96aa9a10a245c5c82dcaff3c85479ff9f1938a6b
Pablo Ferreiro <me@pabloferreiro.es>
2024-04-11 10:16:34 +0200

    sourcemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,10 @@ import solidJs from "@astrojs/solid-js";
 // https://astro.build/config
 export default defineConfig({
   site: import.meta.env.APP_URL,
+  vite: {
+    build: {
+      sourcemap: true
+    }
+  },
   integrations: [solidJs()]
 });
