4e1453f50ce6efb1bf587635d85bc7d02ab9b15b
Pablo Ferreiro <me@pabloferreiro.es>
2025-09-08 17:04:38 +0200

    cleanup

--- src/Console/Reset.php
+++ src/Console/Reset.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Console;
+
 use App\Models\Group;
 use App\Models\Room;
 use App\Models\Schedule;
@@ -48,30 +50,31 @@ class Reset extends Base implements IBase {
     $this->cli->backgroundRed()->out("This action CANNOT BE REVERSED");
     $in = $this->cli->confirm("Continue?");
 
-    if ($in->confirmed()) {
-      $this->cli->out("Wiping...");
-      $schds = Schedule::all();
-      foreach ($schds as $schd) {
-        $schd->delete();
-      }
-
-      $sbjs = Subject::all();
-      foreach ($sbjs as $sbj) {
-        $sbj->delete();
-      }
-
-      $groups = Group::all();
-      foreach ($groups as $group) {
-        $group->delete();
-      }
-
-      $rooms = Room::all();
-      foreach ($rooms as $room) {
-        $room->delete();
-      }
-      $this->cli->out("Finished!");
-    } else {
+    if (!$in->confirmed()) {
       $this->cli->bold()->out("Cancelling!");
+      return;
     }
+
+    $this->cli->out("Wiping...");
+    $schds = Schedule::all();
+    foreach ($schds as $schd) {
+      $schd->delete();
+    }
+
+    $sbjs = Subject::all();
+    foreach ($sbjs as $sbj) {
+      $sbj->delete();
+    }
+
+    $groups = Group::all();
+    foreach ($groups as $group) {
+      $group->delete();
+    }
+
+    $rooms = Room::all();
+    foreach ($rooms as $room) {
+      $room->delete();
+    }
+    $this->cli->out("Finished!");
   }
 }
